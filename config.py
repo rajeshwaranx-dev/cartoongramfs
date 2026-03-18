a895213ebd8ce30e16d94739b89072142fcf572a
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-03-18 21:04:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME </b>\n{uptime}"
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "ʜɪ, ɪ ᴀᴍ ᴍᴀᴅᴇ ʙʏ @trinityXmods\n\nᴏɴʟʏ ᴀᴅᴍɪɴꜱ ᴄᴀɴ ᴜꜱᴇ ᴛʜᴇ ʙᴏᴛ ᴅɪʀᴇᴄᴛʟʏ ʙʏ ꜱᴇɴᴅɪɴɢ ꜰɪʟᴇꜱ — ᴏᴛʜᴇʀꜱ ᴄᴀɴɴᴏᴛ ꜱᴇɴᴅ ᴀɴʏᴛʜɪɴɢ ʜᴇʀᴇ.\n\n📦 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: https://github.com/Trinity-Mods/File-Store-Bot")
 
 # Paste your bot token here (get it from https://t.me/BotFather)
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8620011446:AAHyPnTf67dyQw7OLXujcwE27CsoVm-B2P0") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8145806846:AAH531u2itdnjfyJQGxgFQb8KbcRebcEYco") 
 # Provide the API ID associated with your Telegram application (from https://my.telegram.org/apps)
 APP_ID = int(os.environ.get("APP_ID", "23361081"))
 # Provide the API Hash linked to your Telegram application (available at https://my.telegram.org/apps)
@@ -37,11 +37,11 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002232025730"))
 # Enter your personal Telegram user ID (you can get it by sending '/id' to https://t.me/MissRose_bot).
 OWNER_ID = int(os.environ.get("OWNER_ID", "5371238852"))
 #port set to default 8080, change according to your will.
-PORT = os.environ.get("PORT", "8085")
+PORT = os.environ.get("PORT", "8076")
 # Enter your MongoDB connection URL. You can use a free cloud database from MongoDB Atlas.
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://RajeshLcu2:Rajeshx@cluster0.0razpdy.mongodb.net/?appName=Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://autopost:autopostx@cluster0.3v5ieig.mongodb.net/?appName=Cluster0")
 # Enter the name of your MongoDB database.
-DB_NAME = os.environ.get("DB_NAME", "Leechx")
+DB_NAME = os.environ.get("DB_NAME", "autopost")
 
 # Set the number of worker threads for the bot. To avoid sleeping issues and improve performance, keep it above 50 (recommended).
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
